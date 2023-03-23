a06c82850338d798b435527d0d2717472f92b32f
Jannik H <jannikhegemann1207@gmail.com>
2023-03-23 22:47:47 +0100

    added new methodes to create hazelcastconnection.java

--- src/main/java/dev/jxnnik/hazelcast/api/HazelcastConnection.java
+++ src/main/java/dev/jxnnik/hazelcast/api/HazelcastConnection.java
@@ -1,8 +1,18 @@
 package dev.jxnnik.hazelcast.api;
 
+import com.hazelcast.client.config.SocketOptions;
+
 public class HazelcastConnection extends HazelcastAPI {
 
     public HazelcastConnection(String instanceName, String clusterName, int bufferSize, boolean tcpNoDelay, String... addresses) {
         super(instanceName, clusterName, bufferSize, tcpNoDelay, addresses);
+    }
+
+    public HazelcastConnection(String instanceName, String clusterName, String... addresses) {
+        super(instanceName, clusterName, addresses);
+    }
+
+    public HazelcastConnection(String instanceName, String clusterName, SocketOptions socketOptions, String... addresses) {
+        super(instanceName, clusterName, socketOptions, addresses);
     }
 }
